1141bc7395749eb4f7c06f0aee31e8c75d99c4d9
BlackChampignon <black.champignon@gmail.com>
2026-07-20 18:01:40 +0300

    Different UI modes for desktop and mobile pdf reader for compatability

--- script.js
+++ script.js
@@ -2,9 +2,30 @@ document.addEventListener('DOMContentLoaded', () => {
     const langSelect = document.getElementById('lang');
     const pdfViewer = document.getElementById('pdf-viewer');
 
-    langSelect.addEventListener('change', (event) => {
-        const selectedLang = event.target.value;
+    function getPdfUrl(lang) {
+        // check if the device is mobile based on window width or touch capability
+        const isMobile = window.innerWidth <= 768 || ('ontouchstart' in window);
         
-        pdfViewer.src = `docs/${selectedLang}.pdf`;
+        if (isMobile) {
+            // FitH for mobile devices
+            return `docs/${lang}.pdf#toolbar=0&navpanes=0&view=FitH`;
+        } else {
+            // For desktop
+            return `docs/${lang}.pdf`;
+        }
+    }
+
+    function updatePDF() {
+        const selectedLang = langSelect.value;
+        pdfViewer.src = getPdfUrl(selectedLang);
+    }
+    updatePDF();
+    langSelect.addEventListener('change', updatePDF);
+
+    // If desktop resize
+    let resizeTimeout;
+    window.addEventListener('resize', () => {
+        clearTimeout(resizeTimeout);
+        resizeTimeout = setTimeout(updatePDF, 250);
     });
 });
