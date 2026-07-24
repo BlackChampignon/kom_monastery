fdd83cabdd7d08ff1f47f8b05acb18438d9e0ad9
BlackChampignon <black.champignon@gmail.com>
2026-07-24 13:42:49 +0300

    Only jpg viewer

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const langSelect = document.getElementById('lang');
-    const pdfViewer = document.getElementById('pdf-viewer');
     const imgPage1 = document.getElementById('img-page-1');
     const imgPage2 = document.getElementById('img-page-2');
 
     function updateContent() {
         const selectedLang = langSelect.value;
-        pdfViewer.src = `docs/${selectedLang}.pdf`;
         imgPage1.src = `imgs/${selectedLang}1.jpg`;
         imgPage2.src = `imgs/${selectedLang}2.jpg`;
     }
